dc896112b64b2aee0c00755f719c901fa8cf128b
Abinet505 <abinatmail@gmail.com>
2021-11-21 20:18:40 +0300

    0x03-python-data_structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,8 +17,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
-checkPalindrome(listint_t **headptr, listint_t *tptr);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+size_t listint_len(const listint_t *h);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
