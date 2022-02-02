1d75875c06cb658857721941a6ee49c505d413ea
Abinet505 <abinatmail@gmail.com>
2022-02-02 11:27:38 +0300

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,6 @@
--- list number of records with the same score in the table second table
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+-- Lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT score, COUNT(*) AS 'number'
+FROM second_table
+GROUP BY score
+ORDER BY 'number' DESC;
