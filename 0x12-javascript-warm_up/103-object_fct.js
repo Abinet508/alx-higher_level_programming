c00f88652198b816123860e580e7a5bc70f80e29
Abinet505 <abinatmail@gmail.com>
2022-02-28 21:59:28 +0300

    javascript warm-up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
-
 myObject.incr = function () {
   ++this.value;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
