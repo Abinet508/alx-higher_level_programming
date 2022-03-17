298c815bea8defd6a7e4fc6cef7020713960caf3
Abinet508 <abinatmail@gmail.com>
2022-03-17 21:57:12 +0300

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to '0.0.0.0:5000/catch_me' that causes the server to respond with a message containing 'You got me!', in the body of the response.
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: AlxSchool" -d "user_id=98"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: AlxSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
