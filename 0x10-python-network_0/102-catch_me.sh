a41b6a260d19872fda4c62bc8efb6c113c926885
Abinet508 <abinatmail@gmail.com>
2022-03-17 22:28:06 +0300

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:You got me!"
