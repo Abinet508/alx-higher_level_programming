8fd6fa196cbb15a8bd377aa04e5611ad5e9804f9
Abnet Tesfu Asrat <abinatmail@gmail.com>
2022-03-18 13:26:16 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:You find me!"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
